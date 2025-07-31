d36b20427e3e8a019983bf95a4ce81abbd078da1
soham5505 <sohamkadam5505@gmail.com>
2025-08-01 02:09:25 +0530

    updated portfolio styling and layout

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+  const toggle = document.getElementById('toggle-dark-mode');
+  toggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+  });
+</script>
